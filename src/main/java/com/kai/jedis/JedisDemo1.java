3c471e0a5dab852c18bc228e83aecef83dd6a21a
xiaokai <1152753611@qq.com>
2022-03-22 20:30:49 +0800

    mastercommit_redis4

--- src/main/java/com/kai/jedis/JedisDemo1.java
+++ src/main/java/com/kai/jedis/JedisDemo1.java
@@ -83,6 +83,7 @@ public class JedisDemo1 {
             System.out.println("4");
             System.out.println("5");
             System.out.println("6");
+            System.out.println("push test");
         }}
 
 }
