cf4b415ba8647fe3897d485b3f9c04d7f2f0dd81
xiaokaidhh1 <102038808+xiaokaidhh1@users.noreply.github.com>
2022-03-22 20:40:25 +0800

    Update JedisDemo1.java

--- src/main/java/com/kai/jedis/JedisDemo1.java
+++ src/main/java/com/kai/jedis/JedisDemo1.java
@@ -84,6 +84,7 @@ public class JedisDemo1 {
             System.out.println("5");
             System.out.println("6");
             System.out.println("push test");
+              System.out.println("pull test");
         }}
 
 }
